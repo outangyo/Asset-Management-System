34b7eb088ed3cc3b7f5510d9205f1cc1d26e7097
pongkorn <pongkorn.l169@gmail.com>
2025-08-23 15:12:16 +0700

    Add project files.

---
+++ AssetManagementSystem.Db/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AssetManagementSystem.Db.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AssetManagementSystem.Db.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Asset> Assets { get; set; }
+    }
+}
---
+++ AssetManagementSystem.Db/Entities/Asset.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetManagementSystem.Db.Entities
+{
+    public class Asset
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }   // asset name
+        public string Description { get; set; }  // details about asset
+        public string Category { get; set; }     // e.g., IT, Furniture, Vehicle
+        public string Location { get; set; }     // where the asset is kept
+        public string Department { get; set; }   // which department uses it
+
+    }
+}

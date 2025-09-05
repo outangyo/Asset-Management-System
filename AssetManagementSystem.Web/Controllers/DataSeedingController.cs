9e93212be7ee97801a6e6c67ebc743491763a53c
pongkorn <pongkorn.l169@gmail.com>
2025-09-05 21:48:22 +0700

    add DataSeedingController (API) for dummy data, add dummy data success, create details and edit view success, add IdentityUserSeeder.cs for edit and modify dummy data

---
+++ AssetManagementSystem.Web/Controllers/DataSeedingController.cs
@@ -0,0 +1,23 @@
+﻿using AssetManagementSystem.Db.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssetManagementSystem.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataSeedingController : ControllerBase
+    {
+        private readonly IServiceProvider _services;
+        public DataSeedingController(IServiceProvider services)
+        {
+            _services = services;
+        }
+
+        [HttpPost("seed-dummy-users")]
+        public async Task<IActionResult> SeedDummyUsers()
+        {
+            await IdentityUserSeeder.SeedUsersAsync(_services);
+            return Ok("Dummy users have been seeded successfully.");
+        }
+    }
+}

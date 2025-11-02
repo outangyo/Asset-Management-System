22d58341bfbf138b298f55e951f5ac7bae0cfb8c
pongkorn <pongkorn.l169@gmail.com>
2025-11-02 15:29:46 +0700

    Clean Some Using Code and add Document folder for store Helper Text

--- AssetManagementSystem.Db/Data/ApplicationDbContext.cs
+++ AssetManagementSystem.Db/Data/ApplicationDbContext.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System.Net;
 
 namespace AssetManagementSystem.Db.Data
 {
--- AssetManagementSystem.Db/Entities/ApplicationUser.cs
+++ AssetManagementSystem.Db/Entities/ApplicationUser.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AssetManagementSystem.Db.Entities
 {
